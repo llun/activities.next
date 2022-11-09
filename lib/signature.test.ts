7863ad0a3891af8ffd9e7c96fc243407a7509480
Maythee Anegboonlap <contact@llun.dev>
2022-11-09 22:17:08 +0100

    Add signature verification

--- lib/signature.test.ts
+++ lib/signature.test.ts
@@ -1,10 +1,10 @@
-import { parse } from './signature'
+import { parse, verify } from './signature'
 
 describe('#parse', () => {
-  test('split signature into parts', () => {
+  test('split signature into parts', async () => {
     const signature =
       'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="signature"'
-    expect(parse(signature)).toEqual({
+    expect(await parse(signature)).toEqual({
       keyId: 'https://mastodon.in.th/users/llun#main-key',
       algorithm: 'rsa-sha256',
       headers: '(request-target) host date digest content-type',
@@ -12,19 +12,61 @@ describe('#parse', () => {
     })
   })
 
-  test('filter out non signature header', () => {
-    const signature =
-      'keyId="https://mastodon.in.th/users/llun#main-key",somethingelse,algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="signature"'
-    expect(parse(signature)).toEqual({
-      keyId: 'https://mastodon.in.th/users/llun#main-key',
-      algorithm: 'rsa-sha256',
-      headers: '(request-target) host date digest content-type',
-      signature: 'signature'
-    })
-  })
-
-  test('return empty hash for invalid signature', () => {
+  test('return empty hash for invalid signature', async () => {
     const signature = 'invalid signature'
-    expect(parse(signature)).toEqual({})
+    expect(await parse(signature)).toEqual({})
+  })
+})
+
+describe('#verify', () => {
+  it('returns true when signature and public key is matched', async () => {
+    expect(
+      await verify(
+        {
+          host: 'chat.llun.in.th',
+          'content-length': '2682',
+          'content-type': 'application/activity+json',
+          date: 'Wed, 09 Nov 2022 18:28:37 GMT',
+          digest: 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE=',
+          signature:
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+        },
+        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
+      )
+    ).toBeTruthy()
+  })
+
+  it('returns false when signature and public key is matched but header information is wrong', async () => {
+    expect(
+      await verify(
+        {
+          host: 'chat.llun.in.th',
+          'content-length': '2682',
+          'content-type': 'application/activity+json',
+          date: 'Wed, 09 Nov 2022 18:40:37 GMT',
+          digest: 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE=',
+          signature:
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+        },
+        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
+      )
+    ).toBeFalsy()
+  })
+
+  it('returns false when signature and public key is not matched', async () => {
+    expect(
+      await verify(
+        {
+          host: 'chat.llun.in.th',
+          'content-length': '2682',
+          'content-type': 'application/activity+json',
+          date: 'Wed, 09 Nov 2022 18:28:37 GMT',
+          digest: 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE=',
+          signature:
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+        },
+        'Invalid key'
+      )
+    ).toBeFalsy()
   })
 })
--- lib/signature.ts
+++ lib/signature.ts
@@ -1,9 +1,49 @@
+import crypto from 'crypto'
+import { generate } from 'peggy'
+import fs from 'fs/promises'
+import path from 'path'
+
+interface StringMap {
+  [key: string]: string
+}
+
 const SIGNATURE_KEYS = ['keyId', 'algorithm', 'headers', 'signature']
 
-export function parse(signature: string) {
-  return signature
-    .split(',')
-    .map((item) => item.split('='))
-    .filter((item) => SIGNATURE_KEYS.includes(item[0]))
-    .reduce((out, item) => ({ ...out, [item[0]]: JSON.parse(item[1]) }), {})
+export async function parse(signature: string): Promise<StringMap> {
+  const grammar = await fs.readFile(
+    path.resolve(__dirname, 'signature.pegjs'),
+    'utf-8'
+  )
+  const parser = generate(grammar)
+  try {
+    return (parser.parse(signature) as [string, string][]).reduce(
+      (out, item) => ({ ...out, [item[0]]: item[1] }),
+      {}
+    )
+  } catch {
+    return {}
+  }
+}
+
+// TODO: Add more checks later https://github.com/mastodon/mastodon/blob/main/app/controllers/concerns/signature_verification.rb#L78
+export async function verify(headers: StringMap, publicKey: string) {
+  const headerSignature = await parse(headers.signature)
+  if (!headerSignature.headers) return
+
+  const comparedSignedString = headerSignature.headers
+    .split(' ')
+    .map((item) => {
+      if (item === '(request-target)') return '(request-target): post /inbox'
+      return `${item}: ${headers[item]}`
+    })
+    .join('\n')
+
+  const signature = headerSignature.signature
+  const verifier = crypto.createVerify(headerSignature.algorithm)
+  verifier.update(comparedSignedString)
+  try {
+    return verifier.verify(publicKey, signature, 'base64')
+  } catch {
+    return false
+  }
 }
