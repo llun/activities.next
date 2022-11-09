5e93805ab7ab60e05886864d9a93811148e1d935
Maythee Anegboonlap <contact@llun.dev>
2022-11-09 20:46:45 +0100

    More tests for signature

--- lib/signature.test.ts
+++ lib/signature.test.ts
@@ -1,7 +1,7 @@
 import { parse } from './signature'
 
 describe('#parse', () => {
-  test('split signature into three parts', () => {
+  test('split signature into parts', () => {
     const signature =
       'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="signature"'
     expect(parse(signature)).toEqual({
@@ -10,5 +10,21 @@ describe('#parse', () => {
       headers: '(request-target) host date digest content-type',
       signature: 'signature'
     })
+  })
+
+  test('filter out non signature header', () => {
+    const signature =
+      'keyId="https://mastodon.in.th/users/llun#main-key",somethingelse,algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="signature"'
+    expect(parse(signature)).toEqual({
+      keyId: 'https://mastodon.in.th/users/llun#main-key',
+      algorithm: 'rsa-sha256',
+      headers: '(request-target) host date digest content-type',
+      signature: 'signature'
+    })
+  })
+
+  test('return empty hash for invalid signature', () => {
+    const signature = 'invalid signature'
+    expect(parse(signature)).toEqual({})
   })
 })
