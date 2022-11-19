b54565d178280a53f226ea014006ae33a8b64e93
Maythee Anegboonlap <contact@llun.dev>
2022-11-19 21:23:32 +0100

    Prepare config for dynamodb

--- lib/config.ts
+++ lib/config.ts
@@ -3,9 +3,12 @@ import path from 'path'
 import memoize from 'lodash/memoize'
 import type { Knex } from 'knex'
 
+type KnexBaseDatabase = Knex.Config & { type: 'sqlite3' }
+type DynamoDBDatabase = { type: 'dynamodb' }
+
 export interface Config {
   host: string
-  database: Knex.Config & { type: 'sqlite3' }
+  database: KnexBaseDatabase | DynamoDBDatabase
   allowEmails: string[]
   secretPhase: string
   auth?: {
@@ -17,7 +20,18 @@ export interface Config {
 }
 
 export const getConfig = memoize((): Config => {
-  return JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), 'config.json'), 'utf-8')
-  )
+  try {
+    return JSON.parse(
+      fs.readFileSync(path.resolve(process.cwd(), 'config.json'), 'utf-8')
+    )
+  } catch {
+    // Fall back to read config from environment variable
+    return {
+      host: process.env.ACTIVITIES_HOST || '',
+      database: JSON.parse(process.env.ACTIVITIES_DATABASE || '{}'),
+      allowEmails: JSON.parse(process.env.ACTIVITIES_ALLOW_EMAILS || '[]'),
+      secretPhase: process.env.ACTIVITIES_SECRET_PHASE || '',
+      auth: JSON.parse(process.env.ACTIVITIES_AUTH || '{}')
+    }
+  }
 })
