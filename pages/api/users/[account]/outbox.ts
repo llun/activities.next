c4a15efde616ebc7d967e014745d96c814d4d14d
Maythee Anegboonlap <contact@llun.dev>
2022-11-21 07:22:08 +0100

    Add outbox statuses

--- pages/api/users/[account]/outbox.ts
+++ pages/api/users/[account]/outbox.ts
@@ -1,7 +1,9 @@
 import type { NextApiHandler } from 'next'
+import { OutboxContext } from '../../../../lib/activities/context'
 import { getConfig } from '../../../../lib/config'
 import { ERROR_400, ERROR_404 } from '../../../../lib/errors'
 import { getStorage } from '../../../../lib/storage'
+import { getISOTimeUTC } from '../../../../lib/time'
 
 const handle: NextApiHandler = async (req, res) => {
   const { account, page } = req.query
@@ -13,11 +15,10 @@ const handle: NextApiHandler = async (req, res) => {
 
   switch (req.method) {
     case 'GET': {
+      const actorId = `https://${config.host}/users/${account}`
       if (!page) {
-        const totalItems = await storage.getActorStatusesCount({
-          actorId: account as string
-        })
-        const id = `https://${config.host}/users/${account}/outbox`
+        const totalItems = await storage.getActorStatusesCount({ actorId })
+        const id = `${actorId}/outbox`
         return res.status(200).json({
           '@context': 'https://www.w3.org/ns/activitystreams',
           id,
@@ -26,6 +27,57 @@ const handle: NextApiHandler = async (req, res) => {
           first: `${id}?page=true`,
           last: `${id}?min_id=0&page=true`
         })
+      }
+
+      const statuses = await storage.getActorStatuses({ actorId })
+      return {
+        '@context': OutboxContext,
+        id: `${actorId}/outbox?page=true`,
+        type: 'OrderedCollectionPage',
+        partOf: `${actorId}/outbox`,
+        orderedItems: statuses.map((status) => ({
+          id: `${status.id}/activity`,
+          type: 'Create',
+          actor: actorId,
+          published: getISOTimeUTC(status.createdAt),
+          to: status.to,
+          cc: status.cc,
+          object: {
+            id: status.id,
+            type: status.type,
+            summary: status.summary,
+            inReplyTo: null,
+            published: getISOTimeUTC(status.createdAt),
+            url: `https://${config.host}/@${account}/${new URL(
+              status.id
+            ).pathname
+              .split('/')
+              .pop()}`,
+            attributedTo: status.actorId,
+            to: status.to,
+            cc: status.cc,
+            sensitive: false,
+            atomUri: status.id,
+            inReplyToAtomUri: null,
+            conversation: status.conversation,
+            content: status.text,
+            contentMap: {
+              [status.language || 'en']: status.text
+            },
+            attachment: [],
+            tag: [],
+            replies: {
+              id: status.reply,
+              type: 'Collection',
+              first: {
+                type: 'CollectionPage',
+                next: `${status.reply}?only_other_accounts=true&page=true`,
+                partOf: status.reply,
+                items: []
+              }
+            }
+          }
+        }))
       }
     }
     default:
