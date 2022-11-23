4b8f3b16359f168b8bd0aad71e78646541a9c335
Maythee Anegboonlap <contact@llun.dev>
2022-11-23 20:21:55 +0100

    Prepare area for navigation

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,13 +11,22 @@ import { Button } from '../lib/components/Button'
 import { Header } from '../lib/components/Header'
 import { getConfig } from '../lib/config'
 import { Posts } from '../lib/components/Posts/Posts'
+import {
+  Actor,
+  getHostnameFromId,
+  getUsernameFromId
+} from '../lib/models/actor'
+import styles from './index.module.scss'
+import Link from 'next/link'
 
 interface Props {
   statuses: Status[]
+  actor: Actor
 }
 
-const Page: NextPage<Props> = ({ statuses }) => {
+const Page: NextPage<Props> = ({ actor, statuses }) => {
   const { data: session } = useSession()
+
   return (
     <main>
       <Head>
@@ -25,13 +34,37 @@ const Page: NextPage<Props> = ({ statuses }) => {
       </Head>
       <Header session={session} />
       <section className="container pt-4">
-        <form action="/api/v1/accounts/outbox" method="post">
-          <div className="mb-3">
-            <textarea className="form-control" rows={3} name="message" />
+        <div className="row">
+          <div className="col-12 col-md-3">
+            {actor.iconUrl && (
+              <img
+                alt="Actor icon"
+                className={cn(styles.icon, 'me-4', 'mb-2', 'flex-shrink-0')}
+                src={actor.iconUrl}
+              />
+            )}
+            <h1>{actor.name}</h1>
+            <h4>@{getUsernameFromId(actor.id)}</h4>
+            {Number.isInteger(actor.createdAt) && (
+              <p>
+                Joined{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  dateStyle: 'long',
+                  timeStyle: 'short'
+                }).format(new Date(actor.createdAt))}
+              </p>
+            )}
           </div>
-          <Button type="submit">Send</Button>
-        </form>
-        <Posts statuses={statuses} showActorId showActions />
+          <div className="col-12 col-md-9">
+            <form action="/api/v1/accounts/outbox" method="post">
+              <div className="mb-3">
+                <textarea className="form-control" rows={3} name="message" />
+              </div>
+              <Button type="submit">Send</Button>
+            </form>
+            <Posts statuses={statuses} showActorId showActions />
+          </div>
+        </div>
       </section>
     </main>
   )
@@ -72,10 +105,23 @@ export const getServerSideProps: GetServerSideProps<Props> = async ({
     }
   }
 
-  const statuses = await storage.getStatuses()
+  const [statuses, actor] = await Promise.all([
+    storage.getStatuses(),
+    storage.getActorFromEmail({ email: session.user.email })
+  ])
+  if (!actor) {
+    return {
+      redirect: {
+        destination: '/signin',
+        permanent: false
+      }
+    }
+  }
+
   return {
     props: {
-      statuses
+      statuses,
+      actor
     }
   }
 }
