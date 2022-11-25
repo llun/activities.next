b5a77f76fcb9e5875fedd8c043272199d245d153
Maythee Anegboonlap <contact@llun.dev>
2022-11-25 08:29:44 +0100

    Add link to profile

--- lib/components/Posts/Actor.tsx
+++ lib/components/Posts/Actor.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { FC } from 'react'
 
 import { getHostnameFromId, getUsernameFromId } from '../../models/actor'
@@ -10,9 +11,14 @@ export const Actor: FC<Props> = ({ actorId }) => {
   if (!actorId) return null
   return (
     <div>
-      <strong>
-        @{getUsernameFromId(actorId)}@{getHostnameFromId(actorId)}
-      </strong>
+      <strong>@{getUsernameFromId(actorId)}</strong>
+      <small>@{getHostnameFromId(actorId)}</small>
+      <Link
+        className="ms-2"
+        href={`/@${getUsernameFromId(actorId)}@${getHostnameFromId(actorId)}`}
+      >
+        <i className="bi bi-person-badge"></i>
+      </Link>
     </div>
   )
 }
