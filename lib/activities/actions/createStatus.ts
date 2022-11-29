ece97d64f38d7e2fab21b5db2ab2ca2b3f47498c
Maythee Anegboonlap <contact@llun.dev>
2022-11-29 08:36:54 +0100

    WIP jsonld

--- lib/activities/actions/createStatus.ts
+++ lib/activities/actions/createStatus.ts
@@ -1,3 +1,5 @@
+import * as jsonld from 'jsonld'
+
 import { ContextEntity } from '../entities/base'
 import { Note } from '../entities/note'
 import { Question } from '../entities/question'
@@ -11,4 +13,21 @@ export interface CreateStatus extends BaseActivity, ContextEntity {
   cc: string[]
   object: Note | Question
   signature?: Signature
+}
+
+export const compact = () => {
+  const context = 'https://www.w3.org/ns/activitystreams'
+  const document = {
+    '@type': 'https://www.w3.org/ns/activitystreams#Create',
+    '@id': 'test',
+    'https://www.w3.org/ns/activitystreams#published': '2014-12-12T12:12:12Z',
+    'https://www.w3.org/ns/activitystreams#object': {
+      '@id': 'test/note',
+      '@type': 'https://www.w3.org/ns/activitystreams#Note',
+      'https://www.w3.org/ns/activitystreams#name': 'A Simple Note',
+      'https://www.w3.org/ns/activitystreams#content': 'This is a simple note',
+      'https://www.w3.org/ns/activitystreams#published': '2014-12-12T12:12:12Z'
+    }
+  }
+  return jsonld.compact(document, context)
 }
