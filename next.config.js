ae2034425b618466daa92759e18cf6f274b7368b
Maythee Anegboonlap <contact@llun.dev>
2022-12-08 19:42:55 +0100

    Disable minimize on production

--- next.config.js
+++ next.config.js
@@ -49,6 +49,11 @@ const nextConfig = {
     // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
     // for more information.
     hideSourceMaps: true
+  },
+
+  webpack: (config) => {
+    config.optimization.minimize = false
+    return config
   }
 }
 
