c1121ba81dd40ff27e3d4d331f9b4895ab96b4f4
Maythee Anegboonlap <contact@llun.dev>
2022-12-10 18:47:28 +0100

    Add attachment selector

--- lib/components/PostBox/PostBox.tsx
+++ lib/components/PostBox/PostBox.tsx
@@ -1,4 +1,4 @@
-import { FC, FormEvent, useRef } from 'react'
+import { FC, FormEvent, useRef, useState } from 'react'
 
 import { createStatus } from '../../client'
 import { Media } from '../../medias/apple/media'
@@ -6,7 +6,20 @@ import { Profile } from '../../models/actor'
 import { Status } from '../../models/status'
 import { Button } from '../Button'
 import { AppleGallerButton } from './AppleGalleryButton'
+import styles from './PostBox.module.scss'
 import { ReplyPreview } from './ReplyPreview'
+
+interface AppleGalleryAttachment {
+  type: 'apple'
+  guid: string
+  mediaType: string
+  url: string
+  width: number
+  height: number
+  name?: string
+}
+
+type PostAttachment = AppleGalleryAttachment
 
 interface Props {
   profile: Profile
@@ -21,6 +34,7 @@ export const PostBox: FC<Props> = ({
   onPostCreated,
   onDiscardReply
 }) => {
+  const [attachments, setAttachments] = useState<PostAttachment[]>([])
   const postBoxRef = useRef<HTMLTextAreaElement>(null)
 
   const onPost = async (event: FormEvent<HTMLFormElement>) => {
@@ -46,6 +60,30 @@ export const PostBox: FC<Props> = ({
     postBox.value = ''
   }
 
+  const onSelectAppleMedia = (media: Media) => {
+    // Video is not supported yet
+    if (media.type === 'video') return
+    console.log(media)
+
+    const biggestDerivatives = Object.keys(media.derivatives)
+      .map((value) => parseInt(value, 10))
+      .sort((n1, n2) => n2 - n1)[0]
+    const url = media.derivatives[biggestDerivatives].url
+    if (!url) return
+
+    const attachment: AppleGalleryAttachment = {
+      type: 'apple',
+      guid: media.guid,
+      mediaType: 'image/jpg',
+      name: media.caption,
+      url,
+      width: media.width,
+      height: media.height
+    }
+    console.log(attachments)
+    setAttachments([...attachments, attachment])
+  }
+
   return (
     <div>
       <ReplyPreview status={replyStatus} onClose={onCloseReply} />
@@ -62,12 +100,19 @@ export const PostBox: FC<Props> = ({
           <div>
             <AppleGallerButton
               profile={profile}
-              onSelectMedia={(media: Media) => {
-                console.log(media)
-              }}
+              onSelectMedia={onSelectAppleMedia}
             />
           </div>
           <Button type="submit">Send</Button>
+        </div>
+        <div className={styles.attachments}>
+          {attachments.map((item) => (
+            <div
+              className={styles.attachment}
+              key={item.guid}
+              style={{ backgroundImage: `url(${item.url})` }}
+            />
+          ))}
         </div>
       </form>
     </div>
