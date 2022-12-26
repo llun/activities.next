0403a8d85734e494c294f77ae1be44960a50d1f3
Maythee Anegboonlap <contact@llun.dev>
2022-12-26 20:09:54 +0100

    Add storage seed

---
+++ lib/stub/storage.ts
@@ -0,0 +1,58 @@
+import { ACTIVITY_STREAM_PUBLIC } from '../jsonld/activitystream'
+import { FollowStatus } from '../models/follow'
+import { Storage } from '../storage/types'
+import { seedActor1 } from './seed/actor1'
+import { seedActor2 } from './seed/actor2'
+
+export const getSeedStorage = async (storage: Storage) => {
+  await storage.createAccount(seedActor1)
+  await storage.createAccount(seedActor2)
+
+  const actor1 = await storage.getActorFromEmail({ email: seedActor1.email })
+  const actor2 = await storage.getActorFromEmail({ email: seedActor2.email })
+  if (!actor1 || !actor2) return
+
+  // Actor1 following
+  await storage.createFollow({
+    actorId: actor1.id,
+    targetActorId: 'https://llun.dev/users/test1',
+    inbox: `${actor1.id}/indbox`,
+    sharedInbox: 'https://llun.test/inbox',
+    status: FollowStatus.Accepted
+  })
+  await storage.createFollow({
+    actorId: actor1.id,
+    targetActorId: 'https://llun.dev/users/test2',
+    inbox: `${actor1.id}/indbox`,
+    sharedInbox: 'https://llun.test/inbox',
+    status: FollowStatus.Accepted
+  })
+
+  // Actor2 following
+  await storage.createFollow({
+    actorId: actor2.id,
+    targetActorId: 'https://llun.dev/users/test2',
+    inbox: `${actor2.id}/indbox`,
+    sharedInbox: 'https://llun.test/inbox',
+    status: FollowStatus.Accepted
+  })
+  // Actor2 followers
+  await storage.createFollow({
+    actorId: 'https://llun.dev/users/test1',
+    targetActorId: actor2.id,
+    inbox: 'https://llun.dev/users/test1',
+    sharedInbox: 'https://llun.dev/inbox',
+    status: FollowStatus.Accepted
+  })
+
+  // Actor1 status
+  await storage.createStatus({
+    id: `${actor1.id}/statuses/post-1`,
+    url: `${actor1.id}/statuses/post-1`,
+    actorId: actor1.id,
+    to: [ACTIVITY_STREAM_PUBLIC],
+    cc: [],
+    text: 'This is Actor1 post',
+    type: 'Note'
+  })
+}
