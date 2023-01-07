67080d17c7bf9620810bd3f05c8388cf5d2bdae5
Maythee Anegboonlap <contact@llun.dev>
2023-01-07 19:39:03 +0100

    Add follower url to actor table

---
+++ migrations/20230107183757_add_actor_follower_url.ts
@@ -0,0 +1,13 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('actors', function (table) {
+    table.string('followerUrl')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('actors', function (table) {
+    table.dropColumn('followerUrl')
+  })
+}
