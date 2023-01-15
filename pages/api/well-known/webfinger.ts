5cdc356803381df32642c69761a25950759f7e48
Maythee Anegboonlap <contact@llun.dev>
2023-01-15 09:25:26 +0100

    Fix webfinger for Akkoma

--- pages/api/well-known/webfinger.ts
+++ pages/api/well-known/webfinger.ts
@@ -2,37 +2,43 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 import { Error, WebFinger } from '../../../lib/activities/types'
-import { getConfig } from '../../../lib/config'
 import { ERROR_404 } from '../../../lib/errors'
+import { getStorage } from '../../../lib/storage'
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<WebFinger | Error>
 ) {
-  const config = getConfig()
   const { resource } = req.query
   if (!resource) {
     return res.status(404).json(ERROR_404)
   }
 
-  const account = resource?.slice('acct:'.length, resource.indexOf('@'))
+  const firstResource = Array.isArray(resource) ? resource[0] : resource
+  const account = firstResource.startsWith('acct:')
+    ? firstResource.slice(5)
+    : firstResource
+
+  const [username, domain] = account.split('@')
+  const storage = await getStorage()
+  const actor = await storage?.getActorFromUsername({ username, domain })
+
+  // This is not local actors
+  if (!actor?.privateKey) res.status(404).json(ERROR_404)
 
   res.status(200).json({
-    subject: `acct:${account}@${config.host}`,
-    aliases: [
-      `https://${config.host}/@${account}`,
-      `https://${config.host}/users/llun`
-    ],
+    subject: `acct:${account}@${domain}`,
+    aliases: [`https://${domain}/@${account}`, `https://${domain}/users/llun`],
     links: [
       {
         rel: 'http://webfinger.net/rel/profile-page',
         type: 'text/html',
-        href: `https://${config.host}/@${account}`
+        href: `https://${domain}/@${account}`
       },
       {
         rel: 'self',
         type: 'application/activity+json',
-        href: `https://${config.host}/users/${account}`
+        href: `https://${domain}/users/${account}`
       }
     ]
   })
