522d605ad65161a862148e4182ab430ee0badd16
Maythee Anegboonlap <contact@llun.dev>
2023-01-28 08:24:32 +0100

    Fix JSON error because of GET request to inbox

--- pages/api/inbox.ts
+++ pages/api/inbox.ts
@@ -1,4 +1,4 @@
-import type { NextApiHandler } from 'next'
+import type { NextApiHandler, NextApiResponse } from 'next'
 
 import { announce } from '../../lib/actions/announce'
 import { createNote } from '../../lib/actions/createNote'
@@ -7,6 +7,51 @@ import { activitiesGuard } from '../../lib/guard'
 import { compact } from '../../lib/jsonld'
 import { ERROR_404, ERROR_500 } from '../../lib/responses'
 import { getStorage } from '../../lib/storage'
+import { Storage } from '../../lib/storage/types'
+
+const handlePost = async (
+  storage: Storage,
+  activity: StatusActivity,
+  res: NextApiResponse
+) => {
+  switch (activity.type) {
+    case 'Create': {
+      switch (activity.object.type) {
+        case 'Note': {
+          await createNote({ storage, note: activity.object })
+          break
+        }
+      }
+      return res.status(202).send('')
+    }
+    case 'Announce': {
+      await announce({ storage, status: activity })
+      return res.status(202).send('')
+    }
+    case 'Undo': {
+      switch (activity.object.type) {
+        case 'Announce': {
+          const statusId = activity.object.id
+          await storage.deleteStatus({ statusId })
+          break
+        }
+      }
+      return res.status(202).send('')
+    }
+    case 'Delete': {
+      // TODO: Handle delete object type string
+      if (typeof activity.object === 'string') {
+        return res.status(202).send('')
+      }
+
+      const id = activity.object.id
+      await storage.deleteStatus({ statusId: id })
+      return res.status(202).send('')
+    }
+    default:
+      return res.status(404).send(ERROR_404)
+  }
+}
 
 const ApiHandler: NextApiHandler = activitiesGuard(
   async (req, res) => {
@@ -18,40 +63,9 @@ const ApiHandler: NextApiHandler = activitiesGuard(
       return res.status(500).send(ERROR_500)
     }
 
-    switch (body.type) {
-      case 'Create': {
-        switch (body.object.type) {
-          case 'Note': {
-            await createNote({ storage, note: body.object })
-            break
-          }
-        }
-        return res.status(202).send('')
-      }
-      case 'Announce': {
-        await announce({ storage, status: body })
-        return res.status(202).send('')
-      }
-      case 'Undo': {
-        switch (body.object.type) {
-          case 'Announce': {
-            const statusId = body.object.id
-            await storage.deleteStatus({ statusId })
-            break
-          }
-        }
-        return res.status(202).send('')
-      }
-      case 'Delete': {
-        // TODO: Handle delete object type string
-        if (typeof body.object === 'string') {
-          return res.status(202).send('')
-        }
-
-        const id = body.object.id
-        await storage.deleteStatus({ statusId: id })
-        return res.status(202).send('')
-      }
+    switch (req.method) {
+      case 'POST':
+        return await handlePost(storage, body, res)
       default:
         return res.status(404).send(ERROR_404)
     }
