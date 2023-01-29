0f866613e30c859d3827f17e3be3a905080a1c18
Maythee Anegboonlap <contact@llun.dev>
2023-01-29 10:22:09 +0100

    Add main timeline description

--- lib/timelines/main.ts
+++ lib/timelines/main.ts
@@ -1,5 +1,21 @@
 import { MainTimelineRule, Timeline } from './types'
 
+/**
+ * Main timeline
+ *
+ * Timeline that shows in the main index when user logged in to the server
+ *
+ * Includes
+ * - Self status
+ * - Following people status
+ * - Following people status that replied to another following people status
+ * - Announce from following people
+ *
+ * Excludes
+ * - Following people status that replied to non-following people status
+ * - Non following people status
+ *
+ */
 export const mainTimelineRule: MainTimelineRule = async ({
   currentActor,
   status
