320078f82cdf68f8c41141c9f5575a458d9c71b1
Maythee Anegboonlap <contact@llun.dev>
2023-01-29 10:15:05 +0100

    Add main timeline test

--- lib/timelines/main.test.ts
+++ lib/timelines/main.test.ts
@@ -1,6 +1,11 @@
+import { ACTIVITY_STREAM_PUBLIC } from '../jsonld/activitystream'
+import { Actor } from '../models/actor'
 import { Sqlite3Storage } from '../storage/sqlite3'
 import { mockRequests } from '../stub/activities'
+import { ACTOR3_ID } from '../stub/seed/actor3'
 import { seedStorage } from '../stub/storage'
+import { mainTimelineRule } from './main'
+import { Timeline } from './types'
 
 describe('#mainTimelineRule', () => {
   const storage = new Sqlite3Storage({
@@ -26,7 +31,18 @@ describe('#mainTimelineRule', () => {
     mockRequests(fetchMock)
   })
 
-  it('returns main timeline name for the currentActor status', () => {
-    fail('Not implemented')
+  it('returns main timeline name for the currentActor status', async () => {
+    const actor = (await storage.getActorFromId({ id: ACTOR3_ID })) as Actor
+    const status = await storage.createNote({
+      id: `${ACTOR3_ID}/statuses/post-1`,
+      url: `${ACTOR3_ID}/statuses/post-1`,
+      actorId: ACTOR3_ID,
+      to: [ACTIVITY_STREAM_PUBLIC],
+      cc: [actor?.followersUrl],
+      text: 'This is self status'
+    })
+    expect(
+      await mainTimelineRule({ storage, currentActor: actor, status })
+    ).toEqual(Timeline.MAIN)
   })
 })
