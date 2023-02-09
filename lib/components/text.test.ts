5ff8f502d52afbfe2ee4cb1c2015ba4153c6443e
Maythee Anegboonlap <contact@llun.dev>
2023-02-09 18:52:30 +0100

    Add specs for quote text

---
+++ lib/components/text.test.ts
@@ -0,0 +1,31 @@
+import { convertQuoteToCode } from './text'
+
+describe('#convertQuoteToCode', () => {
+  it('Keep text as it is when there is not quotes', () => {
+    expect(convertQuoteToCode('Test no code block')).toEqual(
+      'Test no code block'
+    )
+  })
+
+  it('replace quote block to code block', () => {
+    expect(convertQuoteToCode('Message with `quote` block')).toEqual(
+      'Message with <code>quote</code> block'
+    )
+  })
+
+  it('replace all quote blocks to code blocks', () => {
+    expect(
+      convertQuoteToCode(
+        'Message with multiple `quote` blocks, e.g. another `quote` here'
+      )
+    ).toEqual(
+      'Message with multiple <div>quote</div> blocks, e.g. another <div>quote</div> here'
+    )
+  })
+
+  it('replace quote with space', () => {
+    expect(
+      convertQuoteToCode('Message with `quote that has space` in it')
+    ).toEqual('Message with <code>quote that has space</code> in it')
+  })
+})
