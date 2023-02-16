8d0f67ee7f98294b62a3ff90e4abc7e407bcab4b
Maythee Anegboonlap <contact@llun.dev>
2023-02-16 21:32:56 +0100

    Prepare TraceSync and TraceAsync

--- lib/trace.ts
+++ lib/trace.ts
@@ -6,24 +6,6 @@ interface Data {
   [key: string]: string | boolean | number | undefined
 }
 
-export const getDatabaseSpan = (
-  databaseMethod: string,
-  collection: string,
-  data: Data = {}
-) => {
-  return Sentry.getCurrentHub()
-    .getScope()
-    ?.getTransaction()
-    ?.startChild({
-      op: 'db',
-      description: databaseMethod,
-      tags: {
-        table: collection
-      },
-      data
-    })
-}
-
 export const getSpan = (op: string, name: string, data: Data = {}) => {
   return Sentry.getCurrentHub().getScope()?.getTransaction()?.startChild({
     op,
@@ -33,6 +15,7 @@ export const getSpan = (op: string, name: string, data: Data = {}) => {
 }
 
 const AsyncFunction = async function () {}.constructor // eslint-disable-line @typescript-eslint/no-empty-function
+type AsyncFunction = typeof AsyncFunction
 
 export function Trace(op: string) {
   return function (
@@ -40,7 +23,7 @@ export function Trace(op: string) {
     propertyKey: string,
     descriptor: PropertyDescriptor
   ) {
-    const original = descriptor.value as typeof AsyncFunction
+    const original = descriptor.value as AsyncFunction
     if (original instanceof AsyncFunction) {
       return {
         ...descriptor,
@@ -64,5 +47,26 @@ export function Trace(op: string) {
         return value
       }
     }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function TraceSync(op: string, fn: Function) {
+  return function (...args: unknown[]) {
+    const span = getSpan(op, fn.name)
+    logger.debug({ op, propertyKey: fn.name })
+    const value = fn(...args)
+    span?.finish()
+    return value
+  }
+}
+
+export function TraceAsync(op: string, fn: AsyncFunction) {
+  return async function (...args: unknown[]) {
+    const span = getSpan(op, fn.name)
+    logger.debug({ op, propertyKey: fn.name })
+    const value = await fn(...args)
+    span?.finish()
+    return value
   }
 }
