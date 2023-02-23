9dd667d8aef79a965477b7f539af34907332e03f
Maythee Anegboonlap <contact@llun.dev>
2023-02-23 22:41:10 +0700

    Add number of choices constraint

--- lib/components/PostBox/PollChoices.tsx
+++ lib/components/PostBox/PollChoices.tsx
@@ -3,6 +3,7 @@ import { FC, useState } from 'react'
 import { Button } from '../Button'
 
 interface Choice {
+  key: number
   text: string
 }
 
@@ -10,39 +11,52 @@ interface Props {
   show: boolean
 }
 
+const key = () => Math.round(Math.random() * 1000)
+
 export const PollChoices: FC<Props> = ({ show }) => {
-  const [choices, setChoices] = useState<Choice[]>([{ text: '' }, { text: '' }])
+  const [choices, setChoices] = useState<Choice[]>([
+    { key: key(), text: '' },
+    { key: key(), text: '' }
+  ])
 
   if (!show) return null
 
   const addChoice = () => {
-    setChoices((previouse) => [...previouse, { text: '' }])
+    if (choices.length > 4) return
+    setChoices((previous) => [...previous, { key: key(), text: '' }])
   }
 
   const removeChoice = (index: number) => {
-    if (choices.length < 2) return
-    console.log(choices[index])
+    if (choices.length < 3) return
     setChoices([...choices.slice(0, index), ...choices.slice(index + 1)])
   }
 
+  console.log(choices)
   return (
     <div>
-      {choices.map((choice, index) => {
-        return (
-          <div key={index} className="mb-1 d-flex flex-row">
-            <input
-              className="form-control"
-              type="text"
-              placeholder={`Choice ${index + 1}`}
-              defaultValue={choice.text}
-            />
-            <Button variant="link" onClick={() => removeChoice(index)}>
-              <i className="bi bi-x-circle-fill" />
-            </Button>
-          </div>
-        )
-      })}
-      <Button onClick={() => addChoice()}>Add choice</Button>
+      {choices.map((choice, index) => (
+        <div key={choice.key} className="mb-1 d-flex flex-row">
+          <input
+            className="form-control"
+            type="text"
+            placeholder={`Choice ${index + 1}`}
+            defaultValue={choice.text}
+            onChange={(e) => {
+              choice.text = e.currentTarget.value
+            }}
+          />
+          <Button
+            disabled={choices.length < 3}
+            variant="link"
+            onClick={() => removeChoice(index)}
+          >
+            <i className="bi bi-x-circle-fill" />
+          </Button>
+        </div>
+      ))}
+      <Button disabled={choices.length >= 5} onClick={() => addChoice()}>
+        Add choice
+      </Button>
     </div>
   )
 }
