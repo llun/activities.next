598b2f36807c1d6790ac67579480ca06c3746d8d
Maythee Anegboonlap <contact@llun.dev>
2023-03-12 11:58:24 +0100

    Disable Sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { withSentryConfig } = require('@sentry/nextjs')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -46,32 +45,4 @@ const nextConfig = {
   }
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  silent: true // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
-
-module.exports =
-  process.env.NODE_ENV === 'production'
-    ? withSentryConfig(
-        {
-          ...nextConfig,
-          sentry: {
-            // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-            // for client-side builds. (This will be the default starting in
-            // `@sentry/nextjs` version 8.0.0.) See
-            // https://webpack.js.org/configuration/devtool/ and
-            // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-            // for more information.
-            hideSourceMaps: true
-          }
-        },
-        sentryWebpackPluginOptions
-      )
-    : nextConfig
+module.exports = nextConfig
