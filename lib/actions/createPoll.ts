19f681c1517d71b2707e84bdbe8acc622e6081b1
Maythee Anegboonlap <contact@llun.dev>
2023-03-20 08:00:46 +0100

    Insert poll into storage

--- lib/actions/createPoll.ts
+++ lib/actions/createPoll.ts
@@ -1,8 +1,10 @@
 import crypto from 'crypto'
+import { encode } from 'html-entities'
 
-import { getMentions } from '../link'
+import { getMentions, linkifyText, paragraphText } from '../link'
 import { Actor } from '../models/actor'
 import { Storage } from '../storage/types'
+import { addStatusToTimelines } from '../timelines'
 import { getSpan } from '../trace'
 import { statusRecipientsCC, statusRecipientsTo } from './createNote'
 
@@ -12,13 +14,15 @@ interface CreatePollFromUserInputParams {
   currentActor: Actor
   choices: string[]
   storage: Storage
+  endAt: number
 }
 export const createPollFromUserInput = async ({
   text,
   replyStatusId,
   currentActor,
   choices = [],
-  storage
+  storage,
+  endAt
 }: CreatePollFromUserInputParams) => {
   const span = getSpan('actions', 'createPollFromUser', {
     replyStatusId
@@ -33,4 +37,37 @@ export const createPollFromUserInput = async ({
 
   const to = statusRecipientsTo(currentActor, replyStatus)
   const cc = statusRecipientsCC(currentActor, mentions, replyStatus)
+
+  const createdPoll = await storage.createPoll({
+    id: statusId,
+    url: `https://${
+      currentActor.domain
+    }/${currentActor.getMention()}/${postId}`,
+    actorId: currentActor.id,
+    text: paragraphText(await linkifyText(encode(text))),
+    summary: '',
+    to,
+    cc,
+    reply: replyStatus?.data.id || '',
+    choices,
+    endAt
+  })
+
+  await Promise.all([
+    addStatusToTimelines(storage, createdPoll),
+    ...mentions.map((mention) =>
+      storage.createTag({
+        statusId,
+        name: mention.name || '',
+        value: mention.href,
+        type: 'mention'
+      })
+    )
+  ])
+
+  const status = await storage.getStatus({ statusId, withReplies: false })
+  if (!status) {
+    span?.finish()
+    return null
+  }
 }
