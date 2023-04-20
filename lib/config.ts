aa6f532dcf66928eb88a9c958cd5e02479287f92
Maythee Anegboonlap <contact@llun.dev>
2023-04-20 21:30:30 +0200

    Add AWS config from environment variable

--- lib/config.ts
+++ lib/config.ts
@@ -39,6 +39,27 @@ export const getConfig = memoize((): Config => {
     )
   } catch {
     // Fall back to read config from environment variable
+    const awsConfig =
+      process.env.AWS_ACCESS_KEY_ID &&
+      process.env.AWS_SECRET_ACCESS_KEY &&
+      process.env.AWS_REGION
+        ? {
+            key: process.env.AWS_ACCESS_KEY_ID,
+            secret: process.env.AWS_SECRET_ACCESS_KEY,
+            region: process.env.AWS_REGION,
+            functions: {
+              ...(process.env.AWS_LAMBDA_SENDMAIL_NAME
+                ? {
+                    sendMail: {
+                      name: process.env.AWS_LAMBDA_SENDMAIL_NAME,
+                      qualifier: process.env.AWS_LAMBDA_SENDMAIL_QUALIFIER
+                    }
+                  }
+                : null)
+            }
+          }
+        : undefined
+
     return {
       host: process.env.ACTIVITIES_HOST || '',
       database: JSON.parse(process.env.ACTIVITIES_DATABASE || '{}'),
@@ -47,7 +68,8 @@ export const getConfig = memoize((): Config => {
       allowMediaDomains: JSON.parse(
         process.env.ACTIVITIES_ALLOW_MEDIA_DOMAINS || '[]'
       ),
-      auth: JSON.parse(process.env.ACTIVITIES_AUTH || '{}')
+      auth: JSON.parse(process.env.ACTIVITIES_AUTH || '{}'),
+      ...awsConfig
     }
   }
 })
