d5e2ad5ee5f3f3c8a995f20e1788a08cc720861c
Maythee Anegboonlap <contact@llun.dev>
2023-05-21 21:08:47 +0200

    Add history popup toggle

--- lib/components/Posts/Actions/EditHistoryButton.tsx
+++ lib/components/Posts/Actions/EditHistoryButton.tsx
@@ -1,7 +1,9 @@
-import { FC } from 'react'
+import cn from 'classnames'
+import { FC, useState } from 'react'
 
 import { StatusData, StatusNote, StatusPoll } from '../../../models/status'
 import { Button } from '../../Button'
+import styles from './EditHistoryButton.module.scss'
 
 interface Props {
   status: StatusNote | StatusPoll
@@ -9,15 +11,28 @@ interface Props {
 }
 
 export const EditHistoryButton: FC<Props> = ({ status, onShowEdits }) => {
+  const [showHistory, setShowHistory] = useState<boolean>(false)
+
   if (status.edits.length === 0) return null
 
   return (
     <Button
+      className={styles.button}
       variant="link"
-      onClick={() => onShowEdits?.(status)}
+      onClick={() => {
+        onShowEdits?.(status)
+        setShowHistory((value) => !value)
+      }}
       title={`${status.edits.length} edits`}
     >
       <i className="bi bi-eraser" />
+      <div
+        className={cn(styles.history, {
+          'd-none': !showHistory
+        })}
+      >
+        HistoryPopup
+      </div>
     </Button>
   )
 }
