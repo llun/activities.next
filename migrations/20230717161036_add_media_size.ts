4d56691efe38df3f177091adeb0edf6774f7f5c3
Maythee Anegboonlap <contact@llun.dev>
2023-07-17 18:55:32 +0200

    Add more medias columns

---
+++ migrations/20230717161036_add_media_size.ts
@@ -0,0 +1,23 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('medias', function (table) {
+    table.string('accountId')
+    table.string('mimeType')
+    table.integer('bytes').unsigned()
+
+    table.index(['accountId', 'mimeType'], 'medias_accountId_mimeType_idx')
+    table.index(['actorId', 'mimeType'], 'medias_actorId_mimeType_idx')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  return knex.schema.alterTable('medias', function (table) {
+    table.dropIndex(['accountId', 'mimeType'], 'medias_accountId_mimeType_idx')
+    table.dropIndex(['actorId', 'mimeType'], 'medias_actorId_mimeType_idx')
+
+    table.dropColumn('accountId')
+    table.dropColumn('mimeType')
+    table.dropColumn('bytes')
+  })
+}
