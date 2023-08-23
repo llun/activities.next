4e6672c5ce77d0493812724efa9e54b9ba45bab6
Maythee Anegboonlap <contact@llun.dev>
2023-08-23 21:15:25 +0200

    Remove AWS config

--- lib/config.ts
+++ lib/config.ts
@@ -35,28 +35,6 @@ export const getConfig = memoize((): Config => {
       fs.readFileSync(path.resolve(process.cwd(), 'config.json'), 'utf-8')
     )
   } catch {
-    // Fall back to read config from environment variable
-    const awsConfig =
-      process.env.AWS_ACCESS_KEY_ID &&
-      process.env.AWS_SECRET_ACCESS_KEY &&
-      process.env.AWS_REGION
-        ? {
-            key: process.env.AWS_ACCESS_KEY_ID,
-            secret: process.env.AWS_SECRET_ACCESS_KEY,
-            region: process.env.AWS_REGION,
-            functions: {
-              ...(process.env.AWS_LAMBDA_SENDMAIL_NAME
-                ? {
-                    sendMail: {
-                      name: process.env.AWS_LAMBDA_SENDMAIL_NAME,
-                      qualifier: process.env.AWS_LAMBDA_SENDMAIL_QUALIFIER
-                    }
-                  }
-                : null)
-            }
-          }
-        : undefined
-
     return {
       host: process.env.ACTIVITIES_HOST || '',
       database: JSON.parse(process.env.ACTIVITIES_DATABASE || '{}'),
@@ -66,7 +44,9 @@ export const getConfig = memoize((): Config => {
         process.env.ACTIVITIES_ALLOW_MEDIA_DOMAINS || '[]'
       ),
       auth: JSON.parse(process.env.ACTIVITIES_AUTH || '{}'),
-      ...awsConfig
+      ...(process.env.ACTIVITIES_EMAIL
+        ? { email: JSON.parse(process.env.ACTIVITIES_EMAIL) }
+        : null)
     }
   }
 })
