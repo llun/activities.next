5ef81db49c8422a22e6882122d078655717ef487
Maythee Anegboonlap <contact@llun.dev>
2023-08-26 21:40:54 +0200

    Send email as notification when someone mention actor

--- lib/timelines/mention.ts
+++ lib/timelines/mention.ts
@@ -1,4 +1,6 @@
+import { getConfig } from '../config'
 import { StatusType } from '../models/status'
+import { sendMail } from '../services/email'
 import { getSpan } from '../trace'
 import { MentionTimelineRule, Timeline } from './types'
 
@@ -10,6 +12,7 @@ export const mentionTimelineRule: MentionTimelineRule = async ({
     actorId: currentActor.id,
     statusId: status.id
   })
+  const config = getConfig()
   if (status.type === StatusType.Announce) {
     span?.finish()
     return null
@@ -21,6 +24,18 @@ export const mentionTimelineRule: MentionTimelineRule = async ({
   }
 
   if (status.text.includes(currentActor.getActorPage())) {
+    const account = currentActor.account
+    if (config.email && account) {
+      await sendMail({
+        from: config.email.serviceFromAddress,
+        to: [account.email],
+        subject: `@${status.actor?.username} mentions you in ${config.host}`,
+        content: {
+          text: `Message: ${status.text}`.trim(),
+          html: status.text
+        }
+      })
+    }
     span?.finish()
     return Timeline.MENTION
   }
