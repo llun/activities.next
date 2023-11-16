5d8aa3fe2eea8527180362f2335b1921bc8173c5
Maythee Anegboonlap <contact@llun.dev>
2023-11-16 09:00:34 +0100

    Use new guard on followers route

--- app/api/users/[username]/followers/route.ts
+++ app/api/users/[username]/followers/route.ts
@@ -1,21 +1,7 @@
-import { NextRequest } from 'next/server'
+import { OnlyLocalUserGuard } from '../guard'
 
-import { ERROR_500 } from '../../../../../lib/errors'
-import { headerHost } from '../../../../../lib/guard'
-import { getStorage } from '../../../../../lib/storage'
-
-type Query = { params: { username: string } }
-
-export const GET = async (req: NextRequest, query: Query) => {
-  const storage = await getStorage()
-  if (!storage) {
-    return Response.json(ERROR_500, { status: 500 })
-  }
-
-  const { username } = query.params
-  const host = headerHost(req.headers)
-  const id = `https://${host}/users/${username}`
-  const followerId = `${id}/followers`
+export const GET = OnlyLocalUserGuard(async (storage, actor) => {
+  const followerId = `${actor.id}/followers`
 
   const totalItems = await storage.getActorFollowersCount({ actorId: id })
   return Response.json({
@@ -24,4 +10,4 @@ export const GET = async (req: NextRequest, query: Query) => {
     type: 'OrderedCollection',
     totalItems
   })
-}
+})
