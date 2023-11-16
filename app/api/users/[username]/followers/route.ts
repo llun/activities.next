bc855c487422aaf291cf67e0c9f6c687df75e213
Maythee Anegboonlap <contact@llun.dev>
2023-11-16 09:05:14 +0100

    Fix missing actor.id in followers route

--- app/api/users/[username]/followers/route.ts
+++ app/api/users/[username]/followers/route.ts
@@ -3,7 +3,7 @@ import { OnlyLocalUserGuard } from '../guard'
 export const GET = OnlyLocalUserGuard(async (storage, actor) => {
   const followerId = `${actor.id}/followers`
 
-  const totalItems = await storage.getActorFollowersCount({ actorId: id })
+  const totalItems = await storage.getActorFollowersCount({ actorId: actor.id })
   return Response.json({
     '@context': 'https://www.w3.org/ns/activitystreams',
     id: followerId,
