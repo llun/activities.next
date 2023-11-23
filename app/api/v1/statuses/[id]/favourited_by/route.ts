556bdcda864d8006c7d660c0ed06a25bb6538149
Maythee Anegboonlap <contact@llun.dev>
2023-11-23 08:50:37 +0100

    Move favourited by to AppRouter

---
+++ app/api/v1/statuses/[id]/favourited_by/route.ts
@@ -0,0 +1,18 @@
+import { ERROR_400 } from '../../../../../../lib/errors'
+import { AuthenticatedGuard } from '../../../../../../lib/guard'
+
+interface Params {
+  id: string
+}
+
+export const GET = AuthenticatedGuard<Params>(async (req, context, params) => {
+  const uuid = params?.params.id
+  if (!uuid) {
+    return Response.json(ERROR_400, { status: 400 })
+  }
+
+  const { currentActor, storage } = context
+  const statusId = `${currentActor.id}/statuses/${uuid}`
+  const actors = await storage.getFavouritedBy({ statusId })
+  return Response.json(actors.map((actor) => actor.toMastodonModel()))
+})
