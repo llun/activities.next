1d4c9687f6978ca3d66fbd5472736c9f11d7983d
Maythee Anegboonlap <contact@llun.dev>
2023-11-23 08:56:33 +0100

    Move update note api to AppRouter

---
+++ app/api/v1/statuses/[id]/route.ts
@@ -0,0 +1,46 @@
+import { z } from 'zod'
+
+import { updateNoteFromUserInput } from '../../../../../lib/actions/updateNote'
+import { ERROR_400, ERROR_403 } from '../../../../../lib/errors'
+import { AuthenticatedGuard } from '../../../../../lib/guard'
+import { getISOTimeUTC } from '../../../../../lib/time'
+
+interface Params {
+  id: string
+}
+
+export const EditNoteSchema = z.object({
+  status: z.string(),
+  spoiler_text: z.string().optional()
+})
+
+export type EditNoteSchema = z.infer<typeof EditNoteSchema>
+
+export const PUT = AuthenticatedGuard<Params>(async (req, context, params) => {
+  const id = params?.params.id
+  if (!id) {
+    return Response.json(ERROR_400, { status: 400 })
+  }
+
+  const { storage, currentActor } = context
+  const statusId = `${currentActor.id}/statuses/${id}`
+  const changes = EditNoteSchema.parse(req.body)
+  const updatedNote = await updateNoteFromUserInput({
+    statusId,
+    currentActor,
+    text: changes.status,
+    summary: changes.spoiler_text,
+    storage
+  })
+
+  if (!updatedNote) {
+    return Response.json(ERROR_403, { status: 403 })
+  }
+  return Response.json({
+    id: updatedNote.id,
+    created_at: getISOTimeUTC(updatedNote.createdAt),
+    in_reply_to_id: updatedNote.reply,
+    edited_at: getISOTimeUTC(updatedNote.updatedAt),
+    content: updatedNote.content
+  })
+})
