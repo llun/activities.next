b3adc0298920d82c51a223ca721a849ea65960da
Maythee Anegboonlap <contact@llun.dev>
2023-12-01 23:25:34 +0100

    Add invalidate cache

--- lib/utils/cache.ts
+++ lib/utils/cache.ts
@@ -1,25 +1,36 @@
 import KeyvRedis from '@keyv/redis'
 import { KeyvRedisOptions } from '@keyv/redis/dist/types'
+import { memoize } from 'lodash'
 
 import { getConfig } from '../config'
+
+const getKeyv = memoize(() => {
+  const config = getConfig()
+  if (!config.redis) {
+    return null
+  }
+
+  const { url, tls } = config.redis
+  const option = tls ? ({ tls: {} } as KeyvRedisOptions) : undefined
+  return new KeyvRedis(url, option)
+})
 
 export const cache = (
   key: string,
   content: unknown,
   ttl: number = 86400000
 ) => {
-  const config = getConfig()
-  if (!config.redis) {
-    return content
-  }
-
-  const { url, tls } = config.redis
-  const option = tls ? ({ tls: {} } as KeyvRedisOptions) : undefined
-  const keyv = new KeyvRedis(url, option)
+  const keyv = getKeyv()
+  if (!keyv) return content
   if (!keyv.get(key)) {
     keyv.set(key, content, ttl)
     return content
   }
-
   return keyv.get(key)
+}
+
+export const invalidate = (key: string) => {
+  const keyv = getKeyv()
+  if (!keyv) return
+  keyv.delete(key)
 }
