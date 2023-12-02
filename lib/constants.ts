324deb6ed2be80bbd9ecfc79e1ba93039c6546a3
Maythee Anegboonlap <contact@llun.dev>
2023-12-02 09:19:43 +0100

    Cache local actor too

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,4 @@ export const ACTIVITIES_SHARED_KEY = 'x-activity-shared-key'
 export const FORWARDED_HOST = 'x-forwarded-host'
 
 export const CACHE_KEY_ACTOR = 'actor_cache'
+export const CACHE_KEY_LOCAL_ACTOR = 'actor_local'
