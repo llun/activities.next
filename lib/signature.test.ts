b45e941e75d64b1e33a1843fc1107ee3407a6df9
Maythee Anegboonlap <contact@llun.dev>
2023-12-22 11:34:14 +0100

    Update signature specs to include standard headers

--- lib/signature.test.ts
+++ lib/signature.test.ts
@@ -37,6 +37,24 @@ describe('#verify', () => {
         '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
       )
     ).toBeTruthy()
+
+    expect(
+      await verify(
+        'post /inbox',
+        new Headers([
+          ['Host', 'chat.llun.in.th'],
+          ['Content-Length', '2682'],
+          ['Content-Type', 'application/activity+json'],
+          ['Date', 'Wed, 09 Nov 2022 18:28:37 GMT'],
+          ['Digest', 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE='],
+          [
+            'Signature',
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+          ]
+        ]),
+        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
+      )
+    ).toBeTruthy()
   })
 
   it('returns false when signature and public key is matched but header information is wrong', async () => {
@@ -55,6 +73,24 @@ describe('#verify', () => {
         '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
       )
     ).toBeFalsy()
+
+    expect(
+      await verify(
+        'post /inbox',
+        new Headers([
+          ['Host', 'chat.llun.in.th'],
+          ['Content-Length', '2682'],
+          ['Content-Type', 'application/activity+json'],
+          ['Date', 'Wed, 09 Nov 2022 18:40:37 GMT'],
+          ['Digest', 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE='],
+          [
+            'Signature',
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+          ]
+        ]),
+        '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA7tttTDSVsia58AR4PQUj\nyqSlwzfQuK/nPnZ4BTWCJTRTvwWg9JXwiIjA2AnQtu/t+qOQgKdKH9yjh84SUtvD\nzAkbt1OTGIQnm5dgAPTGfS17vydxZEPsbhHmJj8UAmU59dgu8QRVl5qoYLSWZyUH\nK9ywrdTJsYkg35NjUjUapY1L7DyMygf7KDlyh0g5ezUufo1cejscbsxomvZTwLZo\nn7cxOeZMFUYw1fsJusbUgQlVHR2qox2cEC6kZGbLvJOiujs7EhRpTjkDFI/DAyNQ\nri4MFXhDg4ozjWcWiKLOsBahVp/iwEm1NF6Mwha6hPhNcInsekzrTQfy1yN7Q+y6\nzQIDAQAB\n-----END PUBLIC KEY-----\n'
+      )
+    ).toBeFalsy()
   })
 
   it('returns false when signature and public key is not matched', async () => {
@@ -70,6 +106,24 @@ describe('#verify', () => {
           signature:
             'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
         },
+        'Invalid key'
+      )
+    ).toBeFalsy()
+
+    expect(
+      await verify(
+        'post /inbox',
+        new Headers([
+          ['Host', 'chat.llun.in.th'],
+          ['Content-Length', '2682'],
+          ['Content-Type', 'application/activity+json'],
+          ['Date', 'Wed, 09 Nov 2022 18:28:37 GMT'],
+          ['Digest', 'SHA-256=ldMA8wZIOUKqGDCdTT9/43jSnnrgO6G3t7zmtphXqyE='],
+          [
+            'Signature',
+            'keyId="https://mastodon.in.th/users/llun#main-key",algorithm="rsa-sha256",headers="(request-target) host date digest content-type",signature="Wx+tR4y1A67nF2fFWOlj8Enx5pzFN3jCo6UB7rpPGTZy0nM4EvuFq1BZgGS08eZJBi+Yf60R1284+YXNQDtkdXM7s66wZQKcmfyKsfSHJGyW5DAQzmxFzCHC/cwTSktCyRc36jUh4OWzKr8wA8vIzexMhTlH5oSOKrTaxPbUzH6vq/uM71oC7fNL29GjZiSJL6q87fPQuKvS7UB0mzBpGb+VfAo7yAp/apMbBXX8iqYL73tJhuTQB5TIOF7GxXLUk6FJ2I7nRQEZXj0/qHA/NISelSNST3ivVH2F1VzeP22K/YPLRSY6zl42JUX3e0zQE4Dln0RvYT971Bw2sMqlig=="'
+          ]
+        ]),
         'Invalid key'
       )
     ).toBeFalsy()
