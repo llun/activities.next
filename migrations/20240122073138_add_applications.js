713d51b12d4369a8c15743f4d87861fbe4d4d16c
Maythee Anegboonlap <contact@llun.dev>
2024-01-22 08:41:34 +0100

    Add add_applications migration

--- migrations/20240122073138_add_applications.js
+++ migrations/20240122073138_add_applications.js
@@ -2,14 +2,24 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
-};
+exports.up = function (knex) {
+  return knex.schema.createTable('applications', function (table) {
+    table.increments('id').primary()
+    table.string('clientName')
+    table.string('secret')
+    table.text('redirectUris')
+    table.text('scopes')
+    table.string('website')
+
+    table.timestamp('createdAt', { useTz: true }).defaultTo(knex.fn.now())
+    table.timestamp('updatedAt', { useTz: true }).defaultTo(knex.fn.now())
+  })
+}
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTable('applications')
+}
