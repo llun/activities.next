3378e3c9c5e5c6921bb4bdf62b9c92c7a3fdcde5
Maythee Anegboonlap <contact@llun.dev>
2024-01-24 17:37:41 +0100

    Make account password nullable

--- lib/models/account.ts
+++ lib/models/account.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 export const Account = z.object({
   id: z.string(),
   email: z.string(),
-  passwordHash: z.string().optional(),
+  passwordHash: z.string().optional().nullable(),
   verificationCode: z.string().nullable().optional(),
 
   createdAt: z.number(),
