c4a78527e5172843dbfdeed1fcb27aff7654ece2
Maythee Anegboonlap <contact@llun.dev>
2024-02-04 22:44:52 +0100

    Rename clientName to name

---
+++ migrations/20240204214039_change_client_name_to_name.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('applications', function (table) {
+    table.renameColumn('clientName', 'name')
+  })
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('applications', function (table) {
+    table.renameColumn('name', 'clientName')
+  })
+}
