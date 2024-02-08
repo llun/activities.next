70edc8cc3704426d4fba6bc98864204c88bbd911
Maythee Anegboonlap <contact@llun.dev>
2024-02-08 09:16:48 +0100

    Fix createApplication api

--- app/api/v1/apps/createApplication.ts
+++ app/api/v1/apps/createApplication.ts
@@ -11,8 +11,8 @@ export const createApplication = async (
 ): Promise<PostResponse> => {
   const scopes = request.scopes ?? 'read'
   try {
-    const existingApplication = await storage.getApplicationFromName({
-      clientName: request.client_name
+    const existingApplication = await storage.getClientFromName({
+      name: request.client_name
     })
     if (existingApplication) {
       return {
@@ -20,14 +20,14 @@ export const createApplication = async (
         id: existingApplication.id,
         client_id: existingApplication.id,
         client_secret: existingApplication.secret,
-        name: existingApplication.clientName,
+        name: existingApplication.name,
         website: existingApplication.website,
         redirect_uri: existingApplication.redirectUris[0]
       }
     }
 
-    const application = await storage.createApplication({
-      clientName: request.client_name,
+    const application = await storage.createClient({
+      name: request.client_name,
       redirectUris: request.redirect_uris.split(' '),
       scopes: scopes.split(' ').map((scope) => Scopes.parse(scope)),
       secret: crypto.randomBytes(16).toString('hex'),
@@ -44,7 +44,7 @@ export const createApplication = async (
       id: application.id,
       client_id: application.id,
       client_secret: application.secret,
-      name: application.clientName,
+      name: application.name,
       website: application.website,
       redirect_uri: application.redirectUris[0]
     }
