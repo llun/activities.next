aa0082395a84b2181be88243d20649c9da0a9f80
Maythee Anegboonlap <contact@llun.dev>
2024-02-09 08:50:06 +0100

    Add convertTextContent specs

--- lib/utils/text/convertTextContent.ts
+++ lib/utils/text/convertTextContent.ts
@@ -4,11 +4,14 @@ import sanitizeHtml from 'sanitize-html'
 import { TagData } from '../../models/tag'
 import { convertEmojisToImages } from './convertEmojisToImages'
 import { convertMarkdownText } from './convertMarkdownText'
+import { linkifyText } from './linkifyText'
 import { SANITIZED_OPTION } from './sanitizeText'
 
 export const convertTextContent = (text: string, tags: TagData[] = []) =>
   _.chain(text)
     .thru((text) => sanitizeHtml(text, SANITIZED_OPTION))
     .thru(convertMarkdownText)
+    .thru(linkifyText)
     .thru(_.curryRight(convertEmojisToImages)(tags))
     .value()
+    .trim()
