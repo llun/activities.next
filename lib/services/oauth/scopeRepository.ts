614318e11b41cabe06394357883cc8af8ece586a
Maythee Anegboonlap <contact@llun.dev>
2024-02-16 22:55:35 +0100

    Add simple scopeRepository

--- lib/services/oauth/scopeRepository.ts
+++ lib/services/oauth/scopeRepository.ts
@@ -1,9 +1,13 @@
 import { OAuthScope, OAuthScopeRepository } from '@jmondi/oauth2-server'
 
+import { Scopes } from '@/lib/storage/types/oauth'
+
 export class ScopeRepository implements OAuthScopeRepository {
   async getAllByIdentifiers(scopeNames: string[]): Promise<OAuthScope[]> {
-    console.log('getAllByIdentifiers', scopeNames)
-    throw new Error('No implementation')
+    const scopes = scopeNames
+      .map((scope) => Scopes.parse(scope))
+      .map((name) => ({ name }))
+    return scopes
   }
 
   async finalize(scopes: OAuthScope[]): Promise<OAuthScope[]> {
