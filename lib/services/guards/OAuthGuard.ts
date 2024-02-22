41e6dce5a0399bddeedb51da4e9f6f4cdc55487d
Maythee Anegboonlap <contact@llun.dev>
2024-02-22 20:45:01 +0100

    Add Bearer parser

--- lib/services/guards/OAuthGuard.ts
+++ lib/services/guards/OAuthGuard.ts
@@ -1,12 +1,24 @@
 import { NextRequest } from 'next/server'
+import { generate } from 'peggy'
 
 import { AppRouterApiHandle, AppRouterParams } from './types'
 
 const BEARER_GRAMMAR = `
-
+value = "Bearer" " " token:base64 { return token }
+base64 = front:(alpha / digit / other)+ padding:"="* { return [front.join(''), padding.join('')].join('') }
+alpha = [a-zA-Z]
+digit = [0-9]
+other = [-._~+/]
 `
+const BEARER_PARSER = generate(BEARER_GRAMMAR)
 
-export const getTokenFromHeader = (authorizationHeader: string) => {}
+export const getTokenFromHeader = (authorizationHeader: string) => {
+  try {
+    return BEARER_PARSER.parse(authorizationHeader)
+  } catch {
+    return null
+  }
+}
 
 export const OAuthGuard =
   <P>(handle: AppRouterApiHandle<P>) =>
