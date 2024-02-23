7e3f9ff9cc3961fd7d6e567bd9931eaf159fefa8
Maythee Anegboonlap <contact@llun.dev>
2024-02-23 16:15:09 +0100

    Add CORS header to timeline api

--- app/api/v1/timelines/[timeline]/route.ts
+++ app/api/v1/timelines/[timeline]/route.ts
@@ -1,32 +1,46 @@
-import { AuthenticatedGuard } from '@/lib/services/guards/AuthenticatedGuard'
+import { OAuthGuard } from '@/lib/services/guards/OAuthGuard'
 import { Timeline } from '@/lib/services/timelines/types'
-import { apiErrorResponse } from '@/lib/utils/response'
+import { Scope } from '@/lib/storage/types/oauth'
+import { HttpMethod } from '@/lib/utils/getCORSHeaders'
+import {
+  apiErrorResponse,
+  apiResponse,
+  defaultOptions
+} from '@/lib/utils/response'
 
+const CORS_HEADERS = [HttpMethod.enum.OPTIONS, HttpMethod.enum.GET]
 const UNSUPPORTED_TIMELINE = [Timeline.LOCAL_PUBLIC]
+
+export const OPTIONS = defaultOptions(CORS_HEADERS)
 
 interface Params {
   timeline: Timeline
 }
 
-export const GET = AuthenticatedGuard<Params>(async (req, context, params) => {
-  const url = new URL(req.url)
-  const startAfterStatusId = url.searchParams.get('startAfterStatusId')
+export const GET = OAuthGuard<Params>(
+  [Scope.enum.read],
+  async (req, context, params) => {
+    const url = new URL(req.url)
+    const startAfterStatusId = url.searchParams.get('startAfterStatusId')
 
-  const { storage, currentActor } = context
-  const { timeline } = params?.params ?? {}
-  if (!timeline) return apiErrorResponse(400)
+    const { storage, currentActor } = context
+    const { timeline } = params?.params ?? {}
+    if (!timeline) return apiErrorResponse(400)
 
-  if (
-    !Object.values(Timeline).includes(timeline) ||
-    UNSUPPORTED_TIMELINE.includes(timeline)
-  ) {
-    return apiErrorResponse(404)
+    if (
+      !Object.values(Timeline).includes(timeline) ||
+      UNSUPPORTED_TIMELINE.includes(timeline)
+    ) {
+      return apiErrorResponse(404)
+    }
+
+    const statuses = await storage.getTimeline({
+      timeline,
+      actorId: currentActor.id,
+      startAfterStatusId
+    })
+    return apiResponse(req, CORS_HEADERS, {
+      statuses: statuses.map((item) => item.toJson())
+    })
   }
-
-  const statuses = await storage.getTimeline({
-    timeline,
-    actorId: currentActor.id,
-    startAfterStatusId
-  })
-  return Response.json({ statuses: statuses.map((item) => item.toJson()) })
-})
+)
