3f5f5d1de54fc2dd53d374f81cfc7016be7ddd13
Maythee Anegboonlap <contact@llun.dev>
2024-03-21 20:19:08 +0100

    Switch to Google Tracer

--- instrumentation.node.ts
+++ instrumentation.node.ts
@@ -1,4 +1,4 @@
-import { TraceExporter as GoogleTraceExporter } from '@google-cloud/opentelemetry-cloud-trace-exporter'
+import GoogleTracer from '@google-cloud/trace-agent'
 import { OTLPTraceExporter as GrpcOLTPTraceExporter } from '@opentelemetry/exporter-trace-otlp-grpc'
 import { OTLPTraceExporter as HttpOLTPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { OTLPTraceExporter as ProtoOLTPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
@@ -26,32 +26,34 @@ const getTraceExporter = (config: Config) => {
       return new GrpcOLTPTraceExporter()
     case 'http/json':
       return new HttpOLTPTraceExporter()
-    case 'google':
-      return new GoogleTraceExporter()
     default:
       return new ProtoOLTPTraceExporter()
   }
 }
 
 const config = getConfig()
-const exporter = getTraceExporter(config)
-if (exporter) {
-  const sdk = new NodeSDK({
-    resource: new Resource({
-      [SEMRESATTRS_SERVICE_NAME]: TRACE_APPLICATION_SCOPE,
-      [SEMRESATTRS_SERVICE_VERSION]: TRACE_APPLICATION_VERSION,
-      [SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV
-    }),
-    spanProcessor: new SimpleSpanProcessor(exporter),
-    instrumentations: [new KnexInstrumentation(), new HttpInstrumentation()]
-  })
-  sdk.start()
+if (config.openTelemetry?.protocol === 'google') {
+  GoogleTracer.start()
+} else {
+  const exporter = getTraceExporter(config)
+  if (exporter) {
+    const sdk = new NodeSDK({
+      resource: new Resource({
+        [SEMRESATTRS_SERVICE_NAME]: TRACE_APPLICATION_SCOPE,
+        [SEMRESATTRS_SERVICE_VERSION]: TRACE_APPLICATION_VERSION,
+        [SEMRESATTRS_DEPLOYMENT_ENVIRONMENT]: process.env.NODE_ENV
+      }),
+      spanProcessor: new SimpleSpanProcessor(exporter),
+      instrumentations: [new KnexInstrumentation(), new HttpInstrumentation()]
+    })
+    sdk.start()
 
-  process.on('SIGTERM', () => {
-    sdk
-      .shutdown()
-      .then(() => console.log('Tracing terminated'))
-      .catch((error) => console.log('Error terminating tracing', error))
-      .finally(() => process.exit(0))
-  })
+    process.on('SIGTERM', () => {
+      sdk
+        .shutdown()
+        .then(() => console.log('Tracing terminated'))
+        .catch((error) => console.log('Error terminating tracing', error))
+        .finally(() => process.exit(0))
+    })
+  }
 }
