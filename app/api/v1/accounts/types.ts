11078f8c9fa11bf87d7cff301820b23207540e70
Maythee Anegboonlap <contact@llun.dev>
2024-06-21 09:06:40 +0200

    Simplify regex

--- app/api/v1/accounts/types.ts
+++ app/api/v1/accounts/types.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const CreateAccountRequest = z.object({
-  username: z.string().regex(/[\w]+/).trim(),
+  username: z.string().regex(/\w+/).trim(),
   email: z.string().email().trim(),
   password: z.string().min(8).trim()
 })
