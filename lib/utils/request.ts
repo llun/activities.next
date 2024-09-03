d111a2d86e369a30a43012f516574678bec09e67
Maythee Anegboonlap <contact@llun.dev>
2024-09-03 18:33:23 +0200

    Use global request timeout instead of response timeout

--- lib/utils/request.ts
+++ lib/utils/request.ts
@@ -37,7 +37,7 @@ export const request = ({
       ...headers
     },
     timeout: {
-      response: defaultResponseTimeout
+      request: DEFAULT_RESPONSE_TIMEOUT
     },
     retry: {
       limit: retryLimit
