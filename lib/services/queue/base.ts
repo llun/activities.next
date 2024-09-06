fffc45e91fa8c9af39ab06822ba7d1b93c348cd8
Maythee Anegboonlap <contact@llun.dev>
2024-09-06 08:02:51 +0200

    Log exception in opentelemetry

--- lib/services/queue/base.ts
+++ lib/services/queue/base.ts
@@ -1,23 +1,38 @@
+import { SpanStatusCode } from '@opentelemetry/api'
+
 import { JOBS } from '@/lib/jobs'
 import { getStorage } from '@/lib/storage'
 import { logger } from '@/lib/utils/logger'
+import { getTracer } from '@/lib/utils/trace'
 
 import { JobMessage } from './type'
 
 export const defaultJobHandle =
   (queueName: string) => async (message: JobMessage) => {
-    logger.debug({ message }, `${queueName} handle job`)
-    const storage = await getStorage()
-    if (!storage) {
-      logger.error('Storage is not available')
-      return
-    }
+    await getTracer().startActiveSpan('queue.handle', async (span) => {
+      logger.debug({ message }, `${queueName} handle job`)
+      const storage = await getStorage()
+      if (!storage) {
+        logger.error('Storage is not available')
+        span.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: 'Storage is not available'
+        })
+        span.end()
+        return
+      }
 
-    const job = JOBS[message.name]
-    if (!job) {
-      logger.error({ message }, 'Unknown job name')
-      return
-    }
+      const job = JOBS[message.name]
+      if (!job) {
+        logger.error({ message }, 'Unknown job name')
+        span.setStatus({
+          code: SpanStatusCode.ERROR,
+          message: 'Unkown job name'
+        })
+        span.end()
+        return
+      }
 
-    await job(storage, message)
+      await job(storage, message)
+    })
   }
