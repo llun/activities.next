1dffd4dbdb3f6921429a0156c07f05d6fa30b2e0
Maythee Anegboonlap <contact@llun.dev>
2024-09-21 19:59:41 +0200

    Extract getJobMessage out

--- app/api/inbox/route.ts
+++ app/api/inbox/route.ts
@@ -1,23 +1,4 @@
-import { ENTITY_TYPE_NOTE, ENTITY_TYPE_QUESTION } from '@llun/activities.schema'
-import isMatch from 'lodash/isMatch'
-import crypto from 'node:crypto'
-
 import { StatusActivity } from '@/lib/activities/actions/status'
-import {
-  AnnounceAction,
-  CreateAction,
-  DeleteAction,
-  UndoAction,
-  UpdateAction
-} from '@/lib/activities/actions/types'
-import {
-  CREATE_ANNOUNCE_JOB_NAME,
-  CREATE_NOTE_JOB_NAME,
-  CREATE_POLL_JOB_NAME,
-  DELETE_OBJECT_JOB_NAME,
-  UPDATE_NOTE_JOB_NAME,
-  UPDATE_POLL_JOB_NAME
-} from '@/lib/jobs/names'
 import { ActivityPubVerifySenderGuard } from '@/lib/services/guards/ActivityPubVerifyGuard'
 import { getQueue } from '@/lib/services/queue'
 import { HttpMethod } from '@/lib/utils/getCORSHeaders'
@@ -29,88 +10,11 @@ import {
   defaultOptions
 } from '@/lib/utils/response'
 
+import { getJobMessage } from './getJobMessage'
+
 const CORS_HEADERS = [HttpMethod.enum.OPTIONS, HttpMethod.enum.POST]
 
 export const OPTIONS = defaultOptions(CORS_HEADERS)
-
-export const getJobMessage = (activity: StatusActivity) => {
-  const deduplicationId = crypto
-    .createHash('sha256')
-    .update(JSON.stringify(activity.id))
-    .digest('hex')
-  if (
-    isMatch(activity, {
-      type: CreateAction,
-      object: { type: ENTITY_TYPE_NOTE }
-    })
-  ) {
-    return {
-      id: deduplicationId,
-      name: CREATE_NOTE_JOB_NAME,
-      data: activity.object
-    }
-  }
-
-  if (
-    isMatch(activity, {
-      type: CreateAction,
-      object: { type: ENTITY_TYPE_QUESTION }
-    })
-  ) {
-    return {
-      id: deduplicationId,
-      name: CREATE_POLL_JOB_NAME,
-      data: activity.object
-    }
-  }
-
-  if (
-    isMatch(activity, {
-      type: UpdateAction,
-      object: { type: ENTITY_TYPE_QUESTION }
-    })
-  ) {
-    return {
-      id: deduplicationId,
-      name: UPDATE_POLL_JOB_NAME,
-      data: activity.object
-    }
-  }
-
-  if (
-    isMatch(activity, {
-      type: UpdateAction,
-      object: { type: ENTITY_TYPE_NOTE }
-    })
-  ) {
-    return {
-      id: deduplicationId,
-      name: UPDATE_NOTE_JOB_NAME,
-      data: activity.object
-    }
-  }
-
-  if (isMatch(activity, { type: AnnounceAction })) {
-    return {
-      id: deduplicationId,
-      name: CREATE_ANNOUNCE_JOB_NAME,
-      data: activity
-    }
-  }
-
-  if (
-    isMatch(activity, { type: UndoAction, object: { type: AnnounceAction } }) ||
-    isMatch(activity, { type: DeleteAction })
-  ) {
-    return {
-      id: deduplicationId,
-      name: DELETE_OBJECT_JOB_NAME,
-      data: activity.object
-    }
-  }
-
-  return null
-}
 
 export const POST = ActivityPubVerifySenderGuard(async (request) => {
   const body = await request.json()
