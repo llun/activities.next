afbb6ccc01eb3fc281fe243f6ab6b495472c9166
Maythee Anegboonlap <contact@llun.dev>
2024-11-17 19:51:32 +0100

    Add standalone build option

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: process.env.BUILD_STANDALONE ? 'standalone' : undefined,
   assetPrefix: '/activities',
   serverExternalPackages: [
     '@aws-sdk/client-lambda',
