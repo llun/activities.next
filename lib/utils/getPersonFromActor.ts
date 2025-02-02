aca4119d7ea9a9829ed3baca63aa89c744f427ac
Maythee Anegboonlap <contact@llun.dev>
2025-02-02 18:20:11 +0100

    Add getPersonFromActor

---
+++ lib/utils/getPersonFromActor.ts
@@ -0,0 +1,49 @@
+import { Person } from '@llun/activities.schema'
+
+import { Actor } from '@/lib/models/actor'
+import { getISOTimeUTC } from '@/lib/utils/getISOTimeUTC'
+
+export const getPersonFromActor = (actor: Actor): Person => {
+  const icon = actor.iconUrl
+    ? {
+        icon: {
+          type: 'Image',
+          mediaType: 'image/jpeg',
+          url: actor.iconUrl
+        }
+      }
+    : null
+  const headerImage = actor.headerImageUrl
+    ? {
+        image: {
+          type: 'Image',
+          mediaType: 'image/png',
+          url: actor.headerImageUrl
+        }
+      }
+    : null
+
+  return Person.parse({
+    id: actor.id,
+    type: 'Person',
+    following: `https://${actor.domain}/users/${actor.username}/following`,
+    followers: `https://${actor.domain}/users/${actor.username}/followers`,
+    inbox: `https://${actor.domain}/users/${actor.username}/inbox`,
+    outbox: `https://${actor.domain}/users/${actor.username}/outbox`,
+    preferredUsername: actor.username,
+    name: actor.name || '',
+    summary: actor.summary || '',
+    url: `https://${actor.domain}/@${actor.username}`,
+    published: getISOTimeUTC(actor.createdAt),
+    publicKey: {
+      id: `${actor.id}#main-key`,
+      owner: actor.id,
+      publicKeyPem: actor.publicKey
+    },
+    endpoints: {
+      sharedInbox: `https://${actor.domain}/inbox`
+    },
+    ...icon,
+    ...headerImage
+  })
+}
