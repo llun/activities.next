36f4e469b9f2e2ea74e24dd797719b2299d46457
Maythee Anegboonlap <contact@llun.dev>
2025-03-05 14:29:44 +0100

    Fix import

--- app/api/v1/accounts/[id]/following/route.ts
+++ app/api/v1/accounts/[id]/following/route.ts
@@ -2,6 +2,7 @@ import { NextRequest } from 'next/server'
 
 import { getDatabase } from '@/lib/database'
 import { Follow } from '@/lib/models/follow'
+import { AppRouterParams } from '@/lib/services/guards/types'
 import { HttpMethod } from '@/lib/utils/getCORSHeaders'
 import {
   apiErrorResponse,
