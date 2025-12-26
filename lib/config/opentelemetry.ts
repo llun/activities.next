70e2f0582850d9c81e428ec856f75d10f0da8bcb
Maythee Anegboonlap <contact@llun.dev>
2025-12-26 09:53:05 +0100

    Allow google otel protocol

--- lib/config/opentelemetry.ts
+++ lib/config/opentelemetry.ts
@@ -2,18 +2,29 @@ import { z } from 'zod'
 
 import { matcher } from './utils'
 
-export const OpenTelemetryProtocol = z.union([
-  z.literal('grpc'),
-  z.literal('http/protobuf'),
-  z.literal('http/json')
+const StandardOpenTelemetryProtocol = z.enum([
+  'grpc',
+  'http/protobuf',
+  'http/json'
 ])
+
+export const OpenTelemetryProtocol = StandardOpenTelemetryProtocol.or(
+  z.literal('google')
+)
 export type OpenTelemetryProtocol = z.infer<typeof OpenTelemetryProtocol>
 
-export const OpenTelemetryConfig = z.object({
-  endpoint: z.string(),
-  protocol: OpenTelemetryProtocol.optional(),
-  headers: z.string().optional()
-})
+export const OpenTelemetryConfig = z.union([
+  z.object({
+    endpoint: z.string(),
+    protocol: StandardOpenTelemetryProtocol.optional(),
+    headers: z.string().optional()
+  }),
+  z.object({
+    protocol: z.literal('google'),
+    endpoint: z.string().optional(),
+    headers: z.string().optional()
+  })
+])
 export type OpenTelemetryConfig = z.infer<typeof OpenTelemetryConfig>
 
 export const getOtelConfig = (): {
@@ -21,21 +32,18 @@ export const getOtelConfig = (): {
 } | null => {
   const hasEnvironmentOtel = matcher('OTEL_EXPORTER_')
   if (!hasEnvironmentOtel) return null
+  const protocol = process.env
+    .OTEL_EXPORTER_OTLP_PROTOCOL as OpenTelemetryProtocol | undefined
   const endpoint =
     process.env.OTEL_EXPORTER_OTLP_ENDPOINT ||
     process.env.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT ||
     process.env.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT ||
     process.env.OTEL_EXPORTER_OTLP_LOGS_ENDPOINT
-  if (!endpoint) return null
+  if (!endpoint && protocol !== 'google') return null
   return {
     openTelemetry: {
-      endpoint,
-      ...(process.env.OTEL_EXPORTER_OTLP_PROTOCOL
-        ? {
-            protocol: process.env
-              .OTEL_EXPORTER_OTLP_PROTOCOL as OpenTelemetryProtocol
-          }
-        : null),
+      ...(endpoint ? { endpoint } : null),
+      ...(protocol ? { protocol } : null),
       ...(process.env.OTEL_EXPORTER_OTLP_HEADERS
         ? { headers: process.env.OTEL_EXPORTER_OTLP_HEADERS }
         : null)
