65118ec0b22060241e0e05fc232989530bdb4e7d
Maythee Anegboonlap <contact@llun.dev>
2026-01-10 21:28:35 +0100

    refactor: use apiResponse helper

--- app/api/v1/accounts/[id]/featured_tags/route.ts
+++ app/api/v1/accounts/[id]/featured_tags/route.ts
@@ -1,12 +1,16 @@
-import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 import { HttpMethod } from '@/lib/utils/getCORSHeaders'
-import { defaultOptions } from '@/lib/utils/response'
+import { apiResponse, defaultOptions } from '@/lib/utils/response'
 
 const CORS_HEADERS = [HttpMethod.enum.OPTIONS, HttpMethod.enum.GET]
 
 export const OPTIONS = defaultOptions(CORS_HEADERS)
 
-export const GET = async () => {
-  return NextResponse.json([], { status: 200 })
+export const GET = async (req: NextRequest) => {
+  return apiResponse({
+    req,
+    allowedMethods: CORS_HEADERS,
+    data: []
+  })
 }
--- app/api/v2/search/route.ts
+++ app/api/v2/search/route.ts
@@ -1,19 +1,20 @@
-import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 
 import { HttpMethod } from '@/lib/utils/getCORSHeaders'
-import { defaultOptions } from '@/lib/utils/response'
+import { apiResponse, defaultOptions } from '@/lib/utils/response'
 
 const CORS_HEADERS = [HttpMethod.enum.OPTIONS, HttpMethod.enum.GET]
 
 export const OPTIONS = defaultOptions(CORS_HEADERS)
 
-export const GET = async () => {
-  return NextResponse.json(
-    {
+export const GET = async (req: NextRequest) => {
+  return apiResponse({
+    req,
+    allowedMethods: CORS_HEADERS,
+    data: {
       accounts: [],
       statuses: [],
       hashtags: []
-    },
-    { status: 200 }
-  )
+    }
+  })
 }
