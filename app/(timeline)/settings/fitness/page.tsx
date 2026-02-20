e2b4cec4d31554b4665707e299a6bcebb98914b2
Maythee Anegboonlap <contact@llun.dev>
2026-02-20 22:09:02 +0100

    Default fitness settings route to files page

--- app/(timeline)/settings/fitness/page.tsx
+++ app/(timeline)/settings/fitness/page.tsx
@@ -3,5 +3,5 @@ import { redirect } from 'next/navigation'
 export const dynamic = 'force-dynamic'
 
 export default function FitnessPage() {
-  redirect('/settings/fitness/strava')
+  redirect('/settings/fitness/files')
 }
