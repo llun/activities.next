aa9a877fb9f74c8f56493c33c15c009cfd27d3d5
Maythee Anegboonlap <contact@llun.dev>
2026-03-02 20:50:47 +0100

    feat: add updateNoteVisibilityFromUserInput action

---
+++ lib/actions/updateNoteVisibility.ts
@@ -0,0 +1,42 @@
+import { Database } from '@/lib/database/types'
+import { statusRecipientsCC, statusRecipientsTo } from '@/lib/actions/createNote'
+import { Actor } from '@/lib/types/domain/actor'
+import { StatusType } from '@/lib/types/domain/status'
+import { MastodonVisibility } from '@/lib/utils/getVisibility'
+import { getSpan } from '@/lib/utils/trace'
+
+interface UpdateNoteVisibilityFromUserInput {
+  statusId: string
+  currentActor: Actor
+  visibility: MastodonVisibility
+  database: Database
+}
+
+export const updateNoteVisibilityFromUserInput = async ({
+  statusId,
+  currentActor,
+  visibility,
+  database
+}: UpdateNoteVisibilityFromUserInput) => {
+  const span = getSpan('actions', 'updateNoteVisibilityFromUser', { statusId })
+  const status = await database.getStatus({ statusId })
+  if (
+    !status ||
+    status.type !== StatusType.enum.Note ||
+    status.actorId !== currentActor.id
+  ) {
+    span.end()
+    return null
+  }
+
+  const to = statusRecipientsTo(currentActor, [], null, visibility)
+  const cc = statusRecipientsCC(currentActor, [], null, visibility)
+  const updatedStatus = await database.updateNoteVisibility({
+    statusId,
+    to,
+    cc
+  })
+
+  span.end()
+  return updatedStatus
+}
