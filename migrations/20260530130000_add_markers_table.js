0d7d7ed6f1e54da6529857073bb3eafce40d33e3
Maythee Anegboonlap <contact@llun.dev>
2026-05-30 19:37:14 +0200

    minor: add markers table migration

---
+++ migrations/20260530130000_add_markers_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = (knex) =>
+  knex.schema.createTable('markers', (table) => {
+    table.string('id').primary()
+    table.string('actorId').notNullable()
+    table.string('timeline').notNullable()
+    table.string('lastReadId').notNullable()
+    table.integer('version').notNullable().defaultTo(1)
+    table.timestamp('updatedAt', { useTz: true }).defaultTo(knex.fn.now())
+
+    table.unique(['actorId', 'timeline'], {
+      indexName: 'markers_actor_timeline_unique'
+    })
+  })
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = (knex) => knex.schema.dropTable('markers')
