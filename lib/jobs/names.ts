a9b5acfad67a4f329a21dd7cb51b34012e3acd8a
Maythee Anegboonlap <contact@llun.dev>
2026-08-29 20:33:21 +0200

    fix: add origin re-fetch job for forwarded inbox activities

--- lib/jobs/names.ts
+++ lib/jobs/names.ts
@@ -1,6 +1,7 @@
 export const CREATE_ANNOUNCE_JOB_NAME = 'CreateAnnounceJob'
 export const CREATE_NOTE_JOB_NAME = 'CreateNoteJob'
 export const RELAY_ANNOUNCE_JOB_NAME = 'RelayAnnounceJob'
+export const PROCESS_FORWARDED_ACTIVITY_JOB_NAME = 'ProcessForwardedActivityJob'
 export const CREATE_POLL_JOB_NAME = 'CreatePollJob'
 export const CREATE_POLL_VOTE_JOB_NAME = 'CreatePollVoteJob'
 export const UPDATE_NOTE_JOB_NAME = 'UpdateNoteJob'
